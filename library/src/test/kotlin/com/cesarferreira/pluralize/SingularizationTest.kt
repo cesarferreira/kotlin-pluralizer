e1f98824c39254d1455a723972859fc401a415c7
Oleg Michailov <themichailov@protonmail.com>
2020-08-05 16:47:24 +0300

    Adding basic unit tests

---
+++ library/src/test/kotlin/com/cesarferreira/pluralize/SingularizationTest.kt
@@ -0,0 +1,23 @@
+package com.cesarferreira.pluralize
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SingularizationTest {
+
+    @Test
+    fun `Should remove default "s" suffix`() {
+        assertEquals("word", "words".singularize())
+    }
+
+    @Test
+    fun `Should handle unCountable words`() {
+        assertEquals("scissors", "scissors".singularize())
+    }
+
+    @Test
+    fun `Should handle exception words`() {
+        assertEquals("person", "people".singularize())
+    }
+
+}
